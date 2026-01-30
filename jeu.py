126114f302ccc50c42d7b2e3fe259d890687b3e8
cristianoMartins17 <cristiano.martins25@orange.fr>
2026-01-30 21:05:04 +0100

    jeu.py et validation.py

--- jeu.py
+++ jeu.py
@@ -1,17 +1,58 @@
-import pygame
+from plateau import Plateau
 
-class Etat_jeu:
+class jeu:
 	def __init__(self):
-		self.echequier = [
-			["Tn", "Cn", "Fn", "Dn", "", "Fn", "Cn", "Tn"],
-			["Pn", "Pn", "Pn", "Pn", "Pn", "Pn", "Pn", "Pn"],
-			["--", "--", "--", "--", "--", "--", "--", "--"],
-			["--", "--", "--", "--", "--", "--", "--", "--"],
-			["--", "--", "--", "--", "--", "--", "--", "--"],
-			["--", "--", "--", "--", "--", "--", "--", "--"],
-			["Pb", "Pb", "Pb", "Pb", "Pb", "Pb", "Pb", "Pb"],
-			["Tb", "Cb", "Fn", "Db", "Rb", "Fn", "Cb", "Tb"]
-		]
+		self.plateau = Plateau()
+		self.tour = 0 # 0 pour blanc, 1 pour noir
+		self.selection = None # Carré sélectionné (ligne, colonne)
+		self.coups_valides = [] # Liste des coups valides pour la pièce sélectionnée
+		self.partie_terminee = False
+		self.gagnant = None # "blanc", "noir" ou None
+  
+	def reinitialiser(self):
+		self.plateau = Plateau()
+		self.tour = 0 # 0 pour blanc, 1 pour noir
+		self.selection = None # Carré sélectionné (ligne, colonne)
+		self.coups_valides = [] # Liste des coups valides pour la pièce sélectionnée
+		self.partie_terminee = False
+		self.gagnant = None # "blanc", "noir" ou None
 
-		self.tourAuBlanc = True
-		self.moveLog = []
+	def selectionner_case(self, case):
+		piece = self.plateau.get_piece(case)
+
+		if piece is None:
+			return False
+
+		if piece.couleur != self.tour:
+			return False
+
+		self.selection = case
+		self.coups_valides = piece.coups_possibles(case, self.plateau)
+		return True
+
+	def jouer_coup(self, depart, arrivee):
+		if arrivee not in self.coups_valides:
+			return False
+
+		self.plateau.deplacer_piece(depart, arrivee)
+		self.selection = None
+		self.coups_valides = []
+
+		self.changer_tour()
+		self.verifier_fin_partie()
+		return True
+
+	def changer_tour(self):
+		if self.tour == 0:
+			self.tour = 1
+		else:
+			self.tour = 0
+   
+	def verifier_fin_partie(self):
+		if self.plateau.roi_capture("blanc"):
+			self.partie_terminee = True
+			self.gagnant = "noir"
+		elif self.plateau.roi_capture("noir"):
+			self.partie_terminee = True
+			self.gagnant = "blanc"
+
--- plateau.py
+++ plateau.py
@@ -1,7 +1,5 @@
 # Initialise le plateau de jeu (grille + pièces)
-
-import pygame
-import os
+from pieces import Pion, Tour, Cavalier, Fou, Dame, Roi
 
 largeur, hauteur = 760, 760
 lignes, colonnes = 8,8
@@ -10,23 +8,38 @@ carre = largeur // lignes
 Brun = (87, 16, 16)
 Blanc = (245, 245, 220)
 
-# Chargement des images des pièces pour qu'elles conviennent à la taille des cases
-Pion_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Pion_Blanc.png")), (carre, carre)) 
-Fou_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Fou_Blanc.png")), (carre, carre)) 
-Cavalier_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Cavalier_Blanc.png")), (carre, carre)) 
-Tour_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Tour_Blanc.png")), (carre, carre)) 
-Dame_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Dame_Blanc.png")), (carre, carre)) 
-Roi_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Roi_Blanc.png")), (carre, carre)) 
-
-Pion_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Pion_noir.png")), (carre, carre)) 
-Fou_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Fou_noir.png")), (carre, carre)) 
-Cavalier_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Cavalier_noir.png")), (carre, carre)) 
-Tour_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Tour_noir.png")), (carre, carre)) 
-Dame_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Dame_noir.png")), (carre, carre)) 
-Roi_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Roi_noir.png")), (carre, carre)) 
-
 class plateau :
     def __init__(self):
         self.grid = [[None for _ in range(8)] for _ in range(8)]
+        self.echequier = [
+			[Tour("noir"), Cavalier("noir"), Fou("noir"), Dame("noir"), Roi("noir"), Fou("noir"), Cavalier("noir"), Tour("noir")],
+			[Pion("noir"), Pion("noir"), Pion("noir"), Pion("noir"), Pion("noir"), Pion("noir"), Pion("noir"), Pion("noir")],
+			[None, None, None, None, None, None, None, None],
+			[None, None, None, None, None, None, None, None],
+			[None, None, None, None, None, None, None, None],
+			[None, None, None, None, None, None, None, None],
+			[Pion("blanc"), Pion("blanc"), Pion("blanc"), Pion("blanc"), Pion("blanc"), Pion("blanc"), Pion("blanc"), Pion("blanc")],
+			[Tour("blanc"), Cavalier("blanc"), Fou("blanc"), Dame("blanc"), Roi("blanc"), Fou("blanc"), Cavalier("blanc"), Tour("blanc")]
+		]
+        
+    def get_piece(self, position):  
+        ligne, colonne = position   # On décompose le tuple position en ligne et colonne
+        return self.echequier[ligne][colonne] # On retourne None ou l'objet Piece à cette position
+
+    def case_vide(self, position):
+        return self.get_piece(position) is None
+
+    def dans_plateau(self, position):   # Vérifie si une position est dans les limites du plateau
+        ligne, colonne = position
+        return 0 <= ligne < 8 and 0 <= colonne < 8 # On retourne True si la position est valide, sinon False
+
+    def deplacer_piece(self, depart, arrivee):
+        piece = self.get_piece(depart)                  # Récupère la pièce à la position de départ
+        self.echequier[arrivee[0]][arrivee[1]] = piece  # Place la pièce à la position d'arrivée
+        self.echequier[depart[0]][depart[1]] = None     # Vide la position de départ 
+
+    def supprimer_piece(self, position):                # Supprime la pièce à la position donnée (sers pour les captures spéciales)
+        self.echequier[position[0]][position[1]] = None
+    
 
         
