b4a114e3fefc00e1c566b4c83c2c53fc987b9047
cristianoMartins17 <cristiano.martins25@orange.fr>
2026-02-09 18:33:27 +0100

    Add new PNG images for game pieces
    - Added Roi_blanc.png and Roi_noir.png for the white and black king pieces.
    - Added Tour_blanc.png and Tour_noir.png for the white and black rook pieces.

---
+++ affichage/__init__.py
@@ -0,0 +1,14 @@
+# Chargement des images des pièces pour qu'elles conviennent à la taille des cases
+Pion_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Pion_Blanc.png")), (carre, carre)) 
+Fou_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Fou_Blanc.png")), (carre, carre)) 
+Cavalier_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Cavalier_Blanc.png")), (carre, carre)) 
+Tour_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Tour_Blanc.png")), (carre, carre)) 
+Dame_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Dame_Blanc.png")), (carre, carre)) 
+Roi_Blanc = pygame.transform.scale(pygame.image.load(os.path.join("images", "Roi_Blanc.png")), (carre, carre)) 
+
+Pion_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Pion_noir.png")), (carre, carre)) 
+Fou_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Fou_noir.png")), (carre, carre)) 
+Cavalier_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Cavalier_noir.png")), (carre, carre)) 
+Tour_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Tour_noir.png")), (carre, carre)) 
+Dame_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Dame_noir.png")), (carre, carre)) 
+Roi_noir = pygame.transform.scale(pygame.image.load(os.path.join("images", "Roi_noir.png")), (carre, carre)) 
